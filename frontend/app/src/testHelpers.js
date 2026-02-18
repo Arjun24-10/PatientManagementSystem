1977159afe8714f2fd15b313dc6df30dc54238d8
Arjun krishnakumar <2004arjunk@gmail.com>
2026-02-18 13:52:56 +0530

    Create testHelpers.js with renderWithProviders and mockAuthUsers helpers

---
+++ frontend/app/src/testHelpers.js
@@ -0,0 +1,7 @@
+export const renderWithProviders = (ui, { providerProps, ...renderOptions } = {}) => {
+    // add implementation for rendering with providers
+};
+
+export const mockAuthUsers = [{
+    // add implementation for mock authentication users
+}];
